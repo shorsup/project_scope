52763a3bd24c224e7285a422e940c8a95d593566
Mitchell Woods <themitchwoods@gmail.com>
2018-06-19 12:26:29 +1000

    Added inital group detection, update on button press only

--- js/main.js
+++ js/main.js
@@ -7,19 +7,32 @@ $(document).ready(function() {
     var editor = ace.edit('editor');
     var input = editor.getValue();
 
+
     editor.session.on('change', function(delta) {
-        calculateTotalHours();
-        milestones();
-        run();
-        groupDetection();
-        Cookies.set('markdown', input);
+        // calculateTotalHours();
+        // milestones();
+        // run();
+        // groupDetection();
+        // Cookies.set('markdown', input);
     });
 
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
     })
 
+    $('.js-calculate').click(function() {
+        calculateTotalHours();
+        milestones();
+        run();
+        groupDetection();
+    });
+
     $('.js-toggle-rt').click(function() {
+        calculateTotalHours();
+        milestones();
+        run();
+        groupDetection();
+
         if (!$('.js-toggle-rt .fa').hasClass('active')) {
             $('#targetDiv').show();
             $('.js-toggle-rt .fa').addClass('active');
@@ -28,6 +41,11 @@ $(document).ready(function() {
     });
 
     $('.js-toggle-md').click(function() {
+        calculateTotalHours();
+        milestones();
+        run();
+        groupDetection();
+        
         if (!$('.js-toggle-md .fa').hasClass('active')) {
             $('#targetDiv').hide();
             $('.js-toggle-md .fa').addClass('active');
@@ -59,17 +77,13 @@ function groupDetection() {
 
     if (matchesGroupBoundaries) {
         for (var i = 0; i < matchesGroupBoundaries.length; i++) {
-            console.log(matchesGroupBoundaries[i]);
+            // console.log(matchesGroupBoundaries[i]);
         }
     }
 
 }
 
-function minutesRegex(regexItem, regexLine, timepoint, type) {
-    // Text Area
-    var editor = ace.edit("editor");
-    var input = editor.getValue();
-    
+function minutesRegex(regexItem, regexLine, timepoint, type, input) {
     // Regex
     var numbers = /\d+/g;
     var regexLineMatch = input.match(regexLine);
@@ -85,7 +99,7 @@ function minutesRegex(regexItem, regexLine, timepoint, type) {
             var itemWithinLine = regexLineMatch[i].match(regexItem);
             var sum = 0;
 
-            console.log('hours ' + itemWithinLine);
+            // console.log('hours ' + itemWithinLine);
 
             if (itemWithinLine) {
                 var hours = parseInt(itemWithinLine[0].match(numbers));
@@ -102,12 +116,12 @@ function minutesRegex(regexItem, regexLine, timepoint, type) {
                 if (regexLineMatch[i].match(/mockup/g) || type === 'baseline mockup') {
                     mockupTotal += sum;
                     sum = 0;
-                    console.log('mockup found: ' + mockupTotal);
+                    // console.log('mockup found: ' + mockupTotal);
                 }
                 if (type === 'baseline wireframe') {
                     wireframeTotal += sum;
                     sum = 0;
-                    console.log('wireframe found: ' + wireframeTotal);
+                    // console.log('wireframe found: ' + wireframeTotal);
                 }
                 codingTotal += sum;
             }
@@ -122,29 +136,76 @@ function calculateTotalHours() {
     var mockupSum = 0;
     var wireframeSum = 0;
 
+    // Input
+    var editor = ace.edit('editor');
+    var input = editor.getValue();
+
+    var segment = input.split('---');
+
     // ---Normal Times---
     // Coding
-    codingSum += minutesRegex(/<!--.\d+(m|min|mins|minutes).-->/g, /.*(<!--.\d+(m|min|mins|minutes).-->).*/g, 'mins', 'normal')[0];
-    codingSum += minutesRegex(/<!--.\d+(hr|hrs|hour|hours).-->/g, /.*(<!--.\d+(hr|hrs|hour|hours).-->).*/g, 'hrs', 'normal')[0];
-    codingSum += minutesRegex(/<!--.\d+(hr|hrs|hour|hours)\s\d+(m|min|mins|minutes).-->/g, /.*(<!--.\d+(hr|hrs|hour|hours)\s\d+(m|min|mins|minutes).-->).*/g, 'hrs/mins', 'normal')[0];
+    codingSum += minutesRegex(/<!--.\d+(m|min|mins|minutes).-->/g, /.*(<!--.\d+(m|min|mins|minutes).-->).*/g, 'mins', 'normal', input)[0];
+    codingSum += minutesRegex(/<!--.\d+(hr|hrs|hour|hours).-->/g, /.*(<!--.\d+(hr|hrs|hour|hours).-->).*/g, 'hrs', 'normal', input)[0];
+    codingSum += minutesRegex(/<!--.\d+(hr|hrs|hour|hours)\s\d+(m|min|mins|minutes).-->/g, /.*(<!--.\d+(hr|hrs|hour|hours)\s\d+(m|min|mins|minutes).-->).*/g, 'hrs/mins', 'normal', input)[0];
     // Mockups
-    mockupSum += minutesRegex(/<!--.\d+(m|min|mins|minutes).-->/g, /.*(<!--.\d+(m|min|mins|minutes).-->).*/g, 'mins', 'normal')[1];
-    mockupSum += minutesRegex(/<!--.\d+(hr|hrs|hour|hours).-->/g, /.*(<!--.\d+(hr|hrs|hour|hours).-->).*/g, 'hrs', 'normal')[1];
-    mockupSum += minutesRegex(/<!--.\d+(hr|hrs|hour|hours)\s\d+(m|min|mins|minutes).-->/g, /.*(<!--.\d+(hr|hrs|hour|hours)\s\d+(m|min|mins|minutes).-->).*/g, 'hrs/mins', 'normal')[1];
+    mockupSum += minutesRegex(/<!--.\d+(m|min|mins|minutes).-->/g, /.*(<!--.\d+(m|min|mins|minutes).-->).*/g, 'mins', 'normal', input)[1];
+    mockupSum += minutesRegex(/<!--.\d+(hr|hrs|hour|hours).-->/g, /.*(<!--.\d+(hr|hrs|hour|hours).-->).*/g, 'hrs', 'normal', input)[1];
+    mockupSum += minutesRegex(/<!--.\d+(hr|hrs|hour|hours)\s\d+(m|min|mins|minutes).-->/g, /.*(<!--.\d+(hr|hrs|hour|hours)\s\d+(m|min|mins|minutes).-->).*/g, 'hrs/mins', 'normal', input)[1];
 
     // ---Baseline Times---
     // Coding
-    codingSum += minutesRegex(/Coding\s(\d+)(m|min|mins|minutes)\n/g, /Coding\s(\d+)(m|min|mins|minutes)\n/g, 'mins', 'baseline')[0];
-    codingSum += minutesRegex(/Coding\s(\d+)(hr|hrs|hour|hours)\n/g, /Coding\s(\d+)(hr|hrs|hour|hours)\n/g, 'hrs', 'baseline')[0];
-    codingSum += minutesRegex(/Coding\s(\d+)(hr|hrs|hour|hours)\s+(\d+)(m|min|mins|minutes)/g, /Coding\s(\d+)(hr|hrs|hour|hours)\s+(\d+)(m|min|mins|minutes)/g, 'hrs/mins', 'baseline')[0];
+    codingSum += minutesRegex(/Coding\s(\d+)(m|min|mins|minutes)\n/g, /Coding\s(\d+)(m|min|mins|minutes)\n/g, 'mins', 'baseline', input)[0];
+    codingSum += minutesRegex(/Coding\s(\d+)(hr|hrs|hour|hours)\n/g, /Coding\s(\d+)(hr|hrs|hour|hours)\n/g, 'hrs', 'baseline', input)[0];
+    codingSum += minutesRegex(/Coding\s(\d+)(hr|hrs|hour|hours)\s+(\d+)(m|min|mins|minutes)/g, /Coding\s(\d+)(hr|hrs|hour|hours)\s+(\d+)(m|min|mins|minutes)/g, 'hrs/mins', 'baseline', input)[0];
     // Wireframes
-    wireframeSum += minutesRegex(/(Wireframe|Wireframes)\s(\d+)(m|min|mins|minutes)/g, /(Wireframe|Wireframes)\s(\d+)(m|min|mins|minutes)/g, 'mins', 'baseline wireframe')[2];
-    wireframeSum += minutesRegex(/(Wireframe|Wireframes)\s(\d+)(hr|hrs|hour|hours)/g, /(Wireframe|Wireframes)\s(\d+)(hr|hrs|hour|hours)/g, 'hrs', 'baseline wireframe')[2];
-    wireframeSum += minutesRegex(/(Wireframe|Wireframes)\s(\d+)(hr|hrs|hour|hours)\s+(\d+)(m|min|mins|minutes)/g, /(Wireframe|Wireframes)\s(\d+)(hr|hrs|hour|hours)\s+(\d+)(m|min|mins|minutes)/g, 'hrs/mins', 'baseline wireframe')[2];
+    wireframeSum += minutesRegex(/(Wireframe|Wireframes)\s(\d+)(m|min|mins|minutes)/g, /(Wireframe|Wireframes)\s(\d+)(m|min|mins|minutes)/g, 'mins', 'baseline wireframe', input)[2];
+    wireframeSum += minutesRegex(/(Wireframe|Wireframes)\s(\d+)(hr|hrs|hour|hours)/g, /(Wireframe|Wireframes)\s(\d+)(hr|hrs|hour|hours)/g, 'hrs', 'baseline wireframe', input)[2];
+    wireframeSum += minutesRegex(/(Wireframe|Wireframes)\s(\d+)(hr|hrs|hour|hours)\s+(\d+)(m|min|mins|minutes)/g, /(Wireframe|Wireframes)\s(\d+)(hr|hrs|hour|hours)\s+(\d+)(m|min|mins|minutes)/g, 'hrs/mins', 'baseline wireframe', input)[2];
     // Mockups
-    mockupSum += minutesRegex(/(Mockup|Mockups)\s(\d+)(m|min|mins|minutes)/g, /(Mockup|Mockups)\s(\d+)(m|min|mins|minutes)/g, 'mins', 'baseline mockup')[1];
-    mockupSum += minutesRegex(/(Mockup|Mockups)\s(\d+)(hr|hrs|hour|hours)/g, /(Mockup|Mockups)\s(\d+)(hr|hrs|hour|hours)/g, 'hrs', 'baseline mockup')[1];
-    mockupSum += minutesRegex(/(Mockup|Mockups)\s(\d+)(hr|hrs|hour|hours)\s+(\d+)(m|min|mins|minutes)/g, /(Mockup|Mockups)\s(\d+)(hr|hrs|hour|hours)\s+(\d+)(m|min|mins|minutes)/g, 'hrs/mins', 'baseline mockup')[1];
+    mockupSum += minutesRegex(/(Mockup|Mockups)\s(\d+)(m|min|mins|minutes)/g, /(Mockup|Mockups)\s(\d+)(m|min|mins|minutes)/g, 'mins', 'baseline mockup', input)[1];
+    mockupSum += minutesRegex(/(Mockup|Mockups)\s(\d+)(hr|hrs|hour|hours)/g, /(Mockup|Mockups)\s(\d+)(hr|hrs|hour|hours)/g, 'hrs', 'baseline mockup', input)[1];
+    mockupSum += minutesRegex(/(Mockup|Mockups)\s(\d+)(hr|hrs|hour|hours)\s+(\d+)(m|min|mins|minutes)/g, /(Mockup|Mockups)\s(\d+)(hr|hrs|hour|hours)\s+(\d+)(m|min|mins|minutes)/g, 'hrs/mins', 'baseline mockup', input)[1];
+
+    for (var i = 0; i < segment.length; i++) {
+        var codingSegment = 0;
+        var wireframeSegment = 0;
+        var mockupSegment = 0;
+        
+        // ---Normal Times---
+        // Coding
+        codingSegment += minutesRegex(/<!--.\d+(m|min|mins|minutes).-->/g, /.*(<!--.\d+(m|min|mins|minutes).-->).*/g, 'mins', 'normal', segment[i])[0];
+        codingSegment += minutesRegex(/<!--.\d+(hr|hrs|hour|hours).-->/g, /.*(<!--.\d+(hr|hrs|hour|hours).-->).*/g, 'hrs', 'normal', segment[i])[0];
+        codingSegment += minutesRegex(/<!--.\d+(hr|hrs|hour|hours)\s\d+(m|min|mins|minutes).-->/g, /.*(<!--.\d+(hr|hrs|hour|hours)\s\d+(m|min|mins|minutes).-->).*/g, 'hrs/mins', 'normal', segment[i])[0];
+        // Mockups
+        mockupSegment += minutesRegex(/<!--.\d+(m|min|mins|minutes).-->/g, /.*(<!--.\d+(m|min|mins|minutes).-->).*/g, 'mins', 'normal', segment[i])[1];
+        mockupSegment += minutesRegex(/<!--.\d+(hr|hrs|hour|hours).-->/g, /.*(<!--.\d+(hr|hrs|hour|hours).-->).*/g, 'hrs', 'normal', segment[i])[1];
+        mockupSegment += minutesRegex(/<!--.\d+(hr|hrs|hour|hours)\s\d+(m|min|mins|minutes).-->/g, /.*(<!--.\d+(hr|hrs|hour|hours)\s\d+(m|min|mins|minutes).-->).*/g, 'hrs/mins', 'normal', segment[i])[1];
+
+        // ---Baseline Times---
+        // Coding
+        codingSegment += minutesRegex(/Coding\s(\d+)(m|min|mins|minutes)\n/g, /Coding\s(\d+)(m|min|mins|minutes)\n/g, 'mins', 'baseline', segment[i])[0];
+        codingSegment += minutesRegex(/Coding\s(\d+)(hr|hrs|hour|hours)\n/g, /Coding\s(\d+)(hr|hrs|hour|hours)\n/g, 'hrs', 'baseline', segment[i])[0];
+        codingSegment += minutesRegex(/Coding\s(\d+)(hr|hrs|hour|hours)\s+(\d+)(m|min|mins|minutes)/g, /Coding\s(\d+)(hr|hrs|hour|hours)\s+(\d+)(m|min|mins|minutes)/g, 'hrs/mins', 'baseline', segment[i])[0];
+        // Wireframes
+        wireframeSegment += minutesRegex(/(Wireframe|Wireframes)\s(\d+)(m|min|mins|minutes)/g, /(Wireframe|Wireframes)\s(\d+)(m|min|mins|minutes)/g, 'mins', 'baseline wireframe', segment[i])[2];
+        wireframeSegment += minutesRegex(/(Wireframe|Wireframes)\s(\d+)(hr|hrs|hour|hours)/g, /(Wireframe|Wireframes)\s(\d+)(hr|hrs|hour|hours)/g, 'hrs', 'baseline wireframe', segment[i])[2];
+        wireframeSegment += minutesRegex(/(Wireframe|Wireframes)\s(\d+)(hr|hrs|hour|hours)\s+(\d+)(m|min|mins|minutes)/g, /(Wireframe|Wireframes)\s(\d+)(hr|hrs|hour|hours)\s+(\d+)(m|min|mins|minutes)/g, 'hrs/mins', 'baseline wireframe', segment[i])[2];
+        // Mockups
+        mockupSegment += minutesRegex(/(Mockup|Mockups)\s(\d+)(m|min|mins|minutes)/g, /(Mockup|Mockups)\s(\d+)(m|min|mins|minutes)/g, 'mins', 'baseline mockup', segment[i])[1];
+        mockupSegment += minutesRegex(/(Mockup|Mockups)\s(\d+)(hr|hrs|hour|hours)/g, /(Mockup|Mockups)\s(\d+)(hr|hrs|hour|hours)/g, 'hrs', 'baseline mockup', segment[i])[1];
+        mockupSegment += minutesRegex(/(Mockup|Mockups)\s(\d+)(hr|hrs|hour|hours)\s+(\d+)(m|min|mins|minutes)/g, /(Mockup|Mockups)\s(\d+)(hr|hrs|hour|hours)\s+(\d+)(m|min|mins|minutes)/g, 'hrs/mins', 'baseline mockup', segment[i])[1];
+
+        var segmentTotal = codingSegment + wireframeSegment + mockupSegment;
+        
+        milestonesInput(segment[i]);
+        
+        if (segmentTotal != 0) {
+            console.log("Total " + segmentTotal);
+            console.log("Wireframe " + wireframeSegment);
+            console.log("Mockup " + mockupSegment);
+            console.log("Coding " + codingSegment);
+        }
+    }
 
     var sum = codingSum + wireframeSum + mockupSum;
     var price = sum * 160;
@@ -171,5 +232,16 @@ function milestones() {
     }
 
     $('.js-milestones').html(x);
-    // (?<=#+).*
+}
+
+function milestonesInput(input) {
+    var milestoneRegex = /\#+.+?((hr|m)\))/g;
+
+    var milestoneMatch = input.match(milestoneRegex);
+
+    if (milestoneMatch) {
+        for (var i = 0; i < milestoneMatch.length; i++) {
+            console.log(milestoneMatch[i].match(/(?!#)(?!\s).+/g));
+        }
+    }
 }
---
+++ test-scope.md
@@ -0,0 +1,260 @@
+# Project Scope
+
+Based on our conversations and the details you've provided, we've put together the below outline of your new design.
+
+All new designs are built on our eCommerce framework, which comes with standard eCommerce features out of the box including shopping cart functionality, secure checkout, product search & filter functionality, customer account pages, and shipping cost calculators. This allows us to focus on creating a design that has a return on investment for your business.
+
+## Aesthetic
+
+- A fully responsive website that looks great on mobiles, tablets and desktop computers.
+- A clean, modern design using the branding colours from your company logo.
+
+---
+
+## General (45m)
+
+- *Please Note: The 'Product Compare/Comparison List' feature from your current site will not be included as it is not supported within Neto.*
+- Create a new category page 'On Sale', which contains all your products currently on sale.<!-- 45m -->
+
+---
+
+## Header (19hr 30m)
+
+### Desktop View
+
+<!-- 
+Coding 4hr
+Mockup 1hr 30m (megamenu)
+-->
+
+![Desktop Header](https://store.neto.com.au/assets/scopes/WHH/header-desktop.png)
+
+We'll create a design of the desktop header & menu for you to approve before implementing the final design. This will include:
+
+- A 'Login/Register' link, once logged in change to 'My Account'.
+- A 'My Wishlist' link, which when logged in takes a user to the wishlist section within the account page.<!-- 15m -->
+- A 'Register For Wholesale Account' button, so that wholesale customers can register accounts.<!-- 15m -->
+- *Please Note: The 'Product Compare/Comparison List' feature from your current site will not be included as it is not supported within Neto.*
+- A clear search field.
+- Details of current cart contents, including a dropdown that allows you to see further info, remove items, go to the cart page or checkout.
+- Your company logo.
+- A category mega menu which shows your top-level categories listed in a horizontal navbar, and sub-categories in a large menu when hovering over each of them. <!-- 2hr 30m -->
+  - This menu has been quoted based on your current category tree, adding a large number of additional categories may break the layout.
+
+![Megamenu](https://store.neto.com.au/assets/scopes/WHH/mega-menu.png)
+
+### Mobile View
+
+<!--
+Wireframe 2hr
+Mockup 3hr 
+Coding 5hr
+-->
+
+![Mobile Header](https://store.neto.com.au/assets/scopes/WHH/header-mobile.png)
+
+We'll create a design of the mobile header & menu for you to approve before implementing the final design. This will include:
+
+- A clean header with your business logo, menu and cart button.
+  - When the menu button is clicked/tapped display the menu in an accordion-style collapsible layout. <!-- 1hr -->
+- A prominent full-width search field.
+
+---
+
+## Home Page (16hr)
+
+<!--
+Wireframe 3hr
+Mockup 5hr 
+Coding 5hr
+-->
+
+We'll create a design of the home page for you to approve before implementing the final design. This page will include:
+
+- A large full-width carousel, with mobile responsive swipe gestures, slide indicator dots, and arrows. <!-- 45m -->
+- 3x top-level category adverts. Placeholder content will be used.<!-- 45m -->
+
+![Home: Categories](https://store.neto.com.au/assets/scopes/WHH/home-categories.png)
+
+- A selection of 'Featured Products' displayed within a carousel, with mobile responsive swipe gestures. <!-- 45m -->
+- A selection of 'New Arrivals' displayed within a carousel, with mobile responsive swipe gestures. <!-- 45m -->
+  - *Please Note: These products will be sorted/displayed based on their upload date to your Neto site, you will not be able to manually change their order. If you would like to change the order, this will required modifying this section within this scope.*
+
+![Home: Featured](https://store.neto.com.au/assets/scopes/WHH/home-featured.png)
+
+---
+
+## Footer (1hr 45m)
+
+<!-- 
+Coding 1hr
+-->
+
+![Footer](https://store.neto.com.au/assets/scopes/WHH/footer.png)
+
+The footer will include the following elements:
+
+- A utility menu, to display links to your information pages.
+- Your phone number and company address, allowing people to easily reach you. <!-- 30m -->
+- A newsletter subscription section, to help with building your mailing list.
+  - *Neto Mail is the default mailing list unless otherwise specified.*
+- A 'Scroll to Top' button.<!-- 15m -->
+
+---
+
+## Category Page (2hr 45m)
+
+<!--
+Coding 2hr
+-->
+
+The Category Page will include the following elements:
+
+- Collapsible sidebar filters which allows the customer to expand/close the sections they’re looking to filter by. <!-- 30m -->
+- Limit the height of the product filters by adding a scroll bar, to reduce the vertical space taken by the filters. <!-- 15m -->
+- *Please Note: The 'Show 12/24 ect.' feature from your current site will not be included as it is not supported within Neto. You will instead be able to update the number of products to be displayed on all categories within the control panel.*
+
+---
+
+## Product Thumbnails (12hr)
+
+### Homepage
+
+<!--
+Mockup (included within homepage)
+Coding (included within homepage)
+-->
+
+![Home: Featured](https://store.neto.com.au/assets/scopes/WHH/home-featured.png)
+
+The design of the Homepage Product Thumbnails will be included within the homepage design, for you to approve before implementing the final design. This thumbnail will include:
+
+- The product image, name and price.
+- When hovering on the product image, display the following:<!-- 30m -->
+  - The buying options button.
+  - A 'Quick View' link that opens a pop-up window outlined below.
+  - *Please Note: The 'Quick View' link will only be displayed on desktop devices.*
+
+### Category
+
+<!--
+Mockup 45m
+Coding 1hr
+-->
+
+![Category: Thumbnails](https://store.neto.com.au/assets/scopes/WHH/category-thumbnails.png)
+
+We'll create a design of the Category Page Product Thumbnail for you to approve before implementing the final design. This popup will include:
+
+- The product image, name, price and the buying options button.
+- An Afterpay advert that displays the breakdown of payments based on the product price. <!-- 30m -->
+- When hovering on the product image, display the following:<!-- 30m -->
+  - A wishlist button.
+  - A 'Quick View' link that opens a pop-up window outlined below.
+  - *Please Note: The 'Quick View' link will only be displayed on desktop devices.*
+
+### Quick View
+
+<!--
+Wireframe 1hr 30m
+Mockup 2hr 30m 
+Coding 3hr 30m
+-->
+
+![Quick View](https://store.neto.com.au/assets/scopes/WHH/quick-view.png)
+
+We'll create a design of the Quick View Popup for you to approve before implementing the final design. This popup will include:
+
+- Main product image
+- Product name
+- Review stars
+- Short description
+- Pricing
+- An Afterpay advert that displays the breakdown of payments based on the product price. <!-- 30m -->
+- Product variations if applicable
+- The quantity input field with plus and minus buttons, which can increase or decrease the quantity value. <!-- 45m -->
+- Buying options
+- A link to the product page to view more information
+
+---
+
+## Product Page (15hr 45m)
+
+<!--
+Wireframe 3hr
+Mockup 4hr 
+Coding 5hr
+-->
+
+![Product Page](https://store.neto.com.au/assets/scopes/WHH/product-page.png)
+
+We'll create a design of the product page for you to approve before implementing the final design. This page will include:
+
+- The product image, name, price and the buying options button.
+- An Afterpay advert that displays the breakdown of payments based on the product price. <!-- 30m -->
+- The quantity input field with plus and minus buttons, which can increase or decrease the quantity value. <!-- 45m -->
+- *Please Note: The 'Tags' feature from your current site will not be included as it is not supported within Neto.*
+- The following USPs (unique selling points), which boost customer confidence (we can change/remove any of these which aren't applicable). Each USP will contain a title, short description and icon. <!-- 1hr -->
+  - Fast Shipping
+  - Lowest Prices
+  - Attentive Support
+- An image based Advertising Group displayed within a carousel, with mobile responsive swipe gestures.<!-- 1hr 30m -->
+
+---
+
+## Important Notes
+
+- Any pages not included in the above scope will be displayed as per our base eCommerce framework, with your branding/colours applied.
+- When a mockup isn't included, we'll use our design expertise when it comes to the final look. If you have specific requirements regarding the aesthetic please let us know before work starts. Alternatively, we can include a billable mockup in the quote, which allows you to request changes to the design.
+- Any functionality not outlined in the above scope, even if it is present on your existing site, any example sites you provided for inspiration, or mentioned in scoping discussions, will not be included as part of the quoted amount below, and may attract additional billable time.
+- Unless specified, changes to the checkout page, print documents (e.g. invoices) or system emails (e.g. order confirmation) will not be included.
+- Theme editor functionality is not available for custom designs.
+
+---
+
+The above work requires **68.5 hours** of design time, which comes to **$10,960** in total (ex.GST). All design projects that have been quoted over 10 hours of work have terms of 50% project due upfront and 50% invoiced after the declared milestone, which for this project is **Category Page: Product Thumbnail Design Deliverable*. Once received, we'll proceed with the next phase of the project.
+
+Please note that this quote is purely for design; it doesn't cover monthly subscription fees or additional services.
+
+## Deliverables
+
+### Home Page Phase
+
+The Home Page Phase is the basis of design for the whole project, therefore it is required to have these pages approved before commencing work on any remaining design deliverables.
+
+We'll aim to get the first draft of the designs completed within the first week.
+
+The design process is collaborative so once you see the first draft we welcome any feedback you may have and we'll collectively work towards having these deliverables approved by week 2.
+
+- 1.1 - Home Page Design Deliverable (week 2)
+
+### Additional Mockup Phase
+
+This phase commences upon the completion of the **Home Page Phase**.
+
+- 1.2 - Mobile Header & Menu Design Deliverable (week 3)
+- 1.3 - Megamenu Design Deliverable (week 3)
+- 1.4 - Product Page Design Deliverable (week 4)
+- 1.5 - Quick View Design Deliverable (week 4)
+- 1.6 - Category Page: Product Thumbnail Design Deliverable (week 4)
+
+### Implementation Phase
+
+Once you've approved all of the design deliverables, we'll move onto implementation. Once complete, we'll deliver the following:
+
+- 2.1 - A link to the finished design, for you to browse and review. (week 8)
+- 2.2 - Custom feature notes, to assist you in working with your new design. (week 8)
+
+<!-- 
+Total Mockup Hours: 26.25hr
+Total Coding Hours: 42.25hr
+- Home Page Implementation 23.5hr
+- Category Page Implementation 2.75hr
+- Product Page Implementation 8.75hr
+- Product Thumbnails Implementation 7.25hr
+- Review and Testing
+-->
+
+At the time of scoping your project requirements it will take **8 weeks** from initial payment to complete the project. This is subject to change if the project isn’t accepted within a week of receiving the proposal to accommodate our existing projects.
+
+After the implementation phase is complete, there tends to be a review period in the following weeks whereby you test the design to ensure that it works well with your data (products, categories, content, etc.) that is being pulled from your Neto control panel. Once this review is complete, your new design will be ready to go live.
