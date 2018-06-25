2ccbae2b2cc9fca1157d9a474ea95c9b9896dcae
Mitchell Woods <themitchwoods@gmail.com>
2018-06-26 00:07:33 +1000

    Markdown converter options

--- js/plugins/script.js
+++ js/plugins/script.js
@@ -3,6 +3,9 @@ function run() {
     var input = editor.getValue();
     target = document.getElementById('targetDiv'),
     converter = new showdown.Converter(),
+    converter.setOption('simplifiedAutoLink', true),
+    converter.setOption('openLinksInNewWindow', true),
+    converter.setOption('emoji', true),
     html = converter.makeHtml(input);
     
     target.innerHTML = html;
