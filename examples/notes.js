a977f732e0988f8e81c3a8bee5136df128a1e9be
Mitchell Woods <themitchwoods@gmail.com>
2018-06-26 18:18:50 +1000

    Print preview

---
+++ examples/notes.js
@@ -0,0 +1,54 @@
+var segments  = [
+    {
+        content: `# Project Scope
+
+            Based on our conversations and the details you've provided, we've put together the below outline of your new design.
+
+            This re-design project will use your existing live theme as a base, with a newly re-designed homepage including desktop header and footer.`
+    },
+    {
+        title: 'Aesthetic',
+        content: `
+            - A fully responsive website that looks great on mobiles, tablets and desktop computers.
+            - A clean, modern design using the branding colours from your company logo.`,
+    }
+]
+
+Loop over segments objects
+Run regex on the content
+
+for (var i = 0; i < segments.length; i++) {
+    i.title 
+    i.content
+}
+
+var calculatedtimes = [
+    {
+        title: 'Aesthetic',
+        time: {
+            h: 7,
+            m: 4.25
+        }
+    },
+    {
+        title: 'Homepage',
+        time: {
+            h: 3,
+            m: 0.25
+        }
+    }
+]
+
+};
+[{
+    title: 'Aesthetic',
+    content: [{
+            text: `A fully responsive website that looks great on mobiles, tablets and desktop computers.`,
+            time: 1.25
+        },
+        {
+            text: `A clean, modern design using the branding colours from your company logo`,
+            time: 3
+        }
+    ]
+}]
